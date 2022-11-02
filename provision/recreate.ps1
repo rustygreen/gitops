3ef8265a76b2a28fefc1cff20a1ccf61c1ac5875
Russell Green <russell.green@rizing.com>
2022-11-02 16:54:50 -0400

    chore: updates

--- provision/recreate.ps1
+++ provision/recreate.ps1
@@ -1,13 +1,21 @@
-./recreate-cluster.ps1
-echo "0) Check flux.";
-flux check --pre;
-echo "1) Bootstrap flux.";
-kubectl apply --kustomize ../base/flux-system/bootstrap;
-echo "2) Create SOPS secret.";
-kubectl -n flux-system create secret generic sops-age --from-file=age.agekey;
-echo "3) Create FluxCD repo credentials secret.";
-sops --decrypt ../clusters/homelab/ssh-key.sops.yaml | kubectl apply -f -;
-echo "4) Link repository";
-kubectl apply --kustomize ../clusters/homelab/production/;
-echo "5) Reconciling";
-./reconcile.ps1
+Write-Host "------ Executing: recreate ------"
+
+& "$PSScriptRoot/recreate-cluster.ps1"
+& Write-Host "------------------------"
+& Write-Host "0) Check flux."
+& flux check --pre
+& Write-Host "------------------------"
+& Write-Host "1) Bootstrap flux."
+& kubectl apply --kustomize "$PSScriptRoot/../base/flux-system/bootstrap"
+& Write-Host "------------------------"
+& Write-Host "2) Create SOPS secret."
+& kubectl -n flux-system create secret generic sops-age --from-file=age.agekey
+& Write-Host "------------------------"
+& Write-Host "3) Create FluxCD repo credentials secret."
+& sops --decrypt "$PSScriptRoot/../clusters/homelab/ssh-key.sops.yaml" | kubectl apply -f -
+& Write-Host "------------------------"
+& Write-Host "4) Link repository"
+& kubectl apply --kustomize "$PSScriptRoot/../clusters/homelab/production/"
+& Write-Host "------------------------"
+& Write-Host "5) Reconciling"
+& "$PSScriptRoot/reconcile.ps1"
