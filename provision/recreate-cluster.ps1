f45e9a76754c9a6f4213cbe6ec5c9e4897c6bb49
Russell Green <russell.green@rizing.com>
2022-11-03 12:39:55 -0400

    chore: updates

--- provision/recreate-cluster.ps1
+++ provision/recreate-cluster.ps1
@@ -2,7 +2,7 @@
 echo "Deleting local cluster";
 k3d cluster delete homelab;
 echo "Creating local cluster";
-k3d cluster create homelab --api-port 6550 -p "8081:80@loadbalancer" --agents 2;
+k3d cluster create homelab --api-port 0.0.0.0:6550 --servers 1 --agents 1 --port "80:80@loadbalancer" --port "443:443@loadbalancer" --k3s-arg "--no-deploy=traefik@server:0";
 
 # minkube
 # echo "Deleting local cluster";
