b83bf91ebb181f19638226e31db5c3c97cd54380
Russell Green <russell.green@rizing.com>
2022-11-04 16:37:03 -0400

    refactor: updates workloads

--- scripts/sync.ps1
+++ scripts/sync.ps1
@@ -1,5 +1,7 @@
 Write-Host "------ Committing & reconciling ------"
 & git add -A
 & git commit -m "refactor: updates workloads"
+& git push
+& git pull
 & "$PSScriptRoot/reconcile.ps1"
 & Write-Host "------------------------------------"
